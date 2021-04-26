b7d2dd22b98169ca2998bd486b9f69ec16a3af7b
Jayesh2925 <66121598+Jayesh2925@users.noreply.github.com>
2021-04-26 11:26:25 +0530

    Add files via upload

---
+++ Python_youtube_downloader.py
@@ -0,0 +1,20 @@
+import pytube
+import sys
+#sys.path.append(r'C:\Users\BINDU\AppData\Roaming\Python\Python39\Scripts')
+from pytube import YouTube #imports youtube video
+link=input("enter the link:  " )
+yt = YouTube(link) #accepts the input from user
+print("Title: ",yt.title)
+print("number of views: ",yt.views)
+print("length of video: ",yt.length,"seconds")  #information about video
+print("Description: ",yt.description)
+print("Ratings: ",yt.rating)
+#print(yt.streams) #prints all available streams
+#print(yt.streams.filter(only_audio=True))
+#print(yt.streams.filter(only_video=True))
+#print(yt.streams.filter(progressive=True))
+
+ys=yt.streams.get_highest_resolution()
+print("Downloading..")
+ys.download()
+print("done!!")
